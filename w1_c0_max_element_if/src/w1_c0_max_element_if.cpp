b1177a30ea2a106d6f06c1a0ba969475ca68a80c
Егор <@192.168.0.196>
2019-03-21 20:13:33 +0500

    New start

--- w1_c0_max_element_if/src/w1_c0_max_element_if.cpp
+++ w1_c0_max_element_if/src/w1_c0_max_element_if.cpp
@@ -1,11 +1,3 @@
-//============================================================================
-// Name        : w1_c0_max_element_if.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
 using namespace std;
 
